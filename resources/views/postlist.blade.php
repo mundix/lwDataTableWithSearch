a0f808addc0e55ef9bf1eebaca28ab1f3fd1d4d0
Edmundo Pichardo <ce.pichardo@gmail.com>
2020-11-01 05:28:21 -0400

    Ready for install livewire

---
+++ resources/views/postlist.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Post List') }}</div>
+
+                <div class="card-body">
+                    The Post List
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
